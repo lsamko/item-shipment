5bda22b7cd728fe3151882b62e4fcd2d05e7cb8a
l.samko <liudmyla@backbase.com>
2022-05-07 20:03:57 +0200

    Initial commit

---
+++ src/main/java/com/example/itemshipment/ItemShipmentApplication.java
@@ -0,0 +1,13 @@
+package com.example.itemshipment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItemShipmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ItemShipmentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/itemshipment/ItemShipmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.itemshipment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItemShipmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
